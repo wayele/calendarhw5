3f5987a9d92855af3630f99dae4ce46a01e3539f
Wub A <ayele.wub@gmail.com>
2020-03-29 22:51:30 -0400

    udated readme with content, js file with for loop

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# calendarhw5
+# Homework 5 - Work Day Scheduler App
+The task is to build a simple calendar application that allows the user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery. This app should also use moment js to work with date and time.
+
+# How it is Made
+
+This web application uses html, css, moment js, bootstrap grids, and dynamic javascript to render a web based daily calendar scheduler to record tasks. The user is has the option to add their tasks by the hour and save with click of a button. A basic html file was created to display the table of a workday hours and an input with text area to include user input. And finally a save button that will save their taks to local storage. Moment js was used to display the current date and time. Any work hours that are past the current time are styled to show background color of gray, current hour is styled to show red, and future hours on scheduler are styled to show green.
+
+# Web Application Components
+
+A html file containing the basic skeleton was used along with custom css, moment js, and a separate javaScript file. Some boostrap components are used to style grids, buttons and other parts of the application.
+
+# Screenshot
+<img src="./scrnshot.png">
+
+#Other details
+git clone git@github.com/wayele/calendarhw5
+
+
+```
+
+## Technologies Used
+
+* Html
+* CSS/Bootstrap
+* JavaScript
+
+## Author
+
+- Wub
--- calendar.js
+++ calendar.js
@@ -1,25 +1,65 @@
-// var time = moment();
-// console.log(time)
-// $('#currentDay').text(time.format("lll"));
+var hrNine = $('#09AM');
+var saveBtnNine = $('#saveBtn-nine');
+var inpValueNine = $('#nine');
 
-var hour = moment().format("h hhA");
-console.log(hour);
-// var currentHour = moment().format('LT');
-var currentHour = moment().hour();
-console.log(currentHour)
 
-var thour = $('.hour');
-console.log(thour);
 
-for (const key in thour) {
-    if (thour[key].id) {
-        console.log(thour[key].id)
-        if (thour[key].id < currentHour) {
-            thour[key].className = "hour past"
-        } else if (thour[key].id > currentHour) {
-            thour[key].className = "hour future"
-        } else {
-            thour[key].className = "hour present"
+$(document).ready(function () {
+    // Moment object to check the current date and time
+    var time = moment();
+    // console.log(time)
+    // Formate current date and time to our preference
+    $('#currentDay').text(time.format("lll"));
+
+    var hour = moment().format("hh mmA");
+    // console.log(hour);
+    // var currentHour = moment().format('LT');
+    var currentHour = moment().hour();
+    // console.log(currentHour)
+
+    var thour = $('.hour');
+    // console.log(thour);
+    // A for loop to check if current hour is less than, equal or greater than our calendar hours and apply style appropriately
+    for (const key in thour) {
+        if (thour[key].id) {
+            // console.log(thour[key].id)
+            if (thour[key].id < currentHour) {
+                thour[key].className = "hour past"
+            } else if (thour[key].id > currentHour) {
+                thour[key].className = "hour future"
+            } else {
+                thour[key].className = "hour present"
+            }
         }
     }
-}
+    // Create an event listener to set items to local storage on click of save
+    $('.saveBtn').on("click", function () {
+        const id = $(this).attr("id")
+        const splitId = id.split("-")
+        const textareaId = "#" + splitId[1]
+        // console.log(textareaId);
+        const value = $(textareaId).val();
+        // If statement to check a value is entered 
+        if (value) {
+            localStorage.setItem(textareaId, value)
+            // console.log(value);
+            // console.log(textareaId);
+        }
+        // console.log($('textarea'))
+
+        // Create an array of IDS that are storing items to local storage
+        var arr = ["#nine", "#ten", "#eleven", "#twelve", "#one", "#two", "#three", "#four", "#five"]
+        // Iterate through the array to display the user inputs to the textbox so it saves during refresh
+        for (let i = 0; i < arr.length; i++) {
+            const task = localStorage.getItem(arr[i]);
+            console.log(task);
+
+            const textAreaOutput = document.getElementById(arr[i]);
+            console.log(textAreaOutput);
+
+        }
+    })
+
+
+})
+// I cannot get the code to display what is being saved in local storage. Will need help understanding local storage and its functionality better.
